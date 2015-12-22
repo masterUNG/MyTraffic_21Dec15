f90cb38d63c1dd9a91f509f5e8529d4c0524594f
masterUNG <phrombutr@gmail.com>
2015-12-22 13:42:40 +0700

    Create ListView finish

---
+++ app/src/main/java/appewtc/masterung/mytraffic/MyAdapter.java
@@ -0,0 +1,63 @@
+package appewtc.masterung.mytraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by masterUNG on 12/22/15 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private String[] titleStrings, detailStrings;
+    private int[] iconInts;
+
+    public MyAdapter(Context objContext, String[] titleStrings, String[] detailStrings, int[] iconInts) {
+        this.objContext = objContext;
+        this.titleStrings = titleStrings;
+        this.detailStrings = detailStrings;
+        this.iconInts = iconInts;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview, viewGroup, false);
+
+        //For Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titleTextView.setText(titleStrings[i]);
+
+        //For Detail
+        TextView detailTextView = (TextView) objView1.findViewById(R.id.textView3);
+        detailTextView.setText(detailStrings[i]);
+
+        //For Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
+
+        return objView1;
+    }
+}   // Main Class
